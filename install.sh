ef5de9dc7eadc9cfa10af46fc60092e69906e5c1
aser-as <65462446+aser-as@users.noreply.github.com>
2020-05-16 23:17:37 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/data/data/com.termux/files/usr/bin
+clear
+echo "HiddenEye installer (Termux) @ASER"
+cd $HOME
+sleep 2
+echo " "
+echo "#Requesting packages..."
+pkg install git python php curl openssh
+echo " "
+echo "#Cloning HiddenEye..."
+git clone -b Termux-Support-Branch https://github.com/DarkSecDevelopers/HiddenEye.git
+chmod 777 HiddenEye
+echo " "
+echo "#Installing HiddenEye..."
+pip install requests
+cd $HOME/HEInstaller
+chmod -x HiddenEye
+mv HiddenEye /data/data/com.termux/files/usr/bin
+clear
+echo "HiddenEye has been install type HiddenEye to run!"
